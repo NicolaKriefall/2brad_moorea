24e8a3e5c3727cf7b04b6cb11a7910dd0c5ce442
NicolaKriefall <thenicolakriefall@gmail.com>
2019-09-20 13:46:16 -0400

    working up heterozygosity stuffs

--- part5_diversity.R
+++ part5_diversity.R
@@ -1,10 +1,9 @@
-df <- read.table("df")
+df <- read.table("part5_het_out.txt")
 df$het <- df$V3/(df$V2+df$V3)
 df$site <- substr(df$V1, 0, 4)
 df[86:137,5] <- substr(df[86:137,5], 0, 2)
 str(df)
 nocrap <- df[2:137,]
-plot(het~site,data=nocrap)
 hetz <- nocrap$het
 sitez <- nocrap$site
 dfmaybe <- data.frame(sitez,hetz)
